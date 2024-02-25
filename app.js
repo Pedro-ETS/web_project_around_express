0ac8a209abea643af994aa5761969e337dde072a
Pedro-ETS <pedrotorrestec@gmail.com>
2024-02-25 00:03:43 -0600

    Re-rastreo de cambios en backend/web_project_around_express

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const auth = require('./middleware/auth');
-const { celebrate, Joi } = require('celebrate');
-const validator = require('validator'); 
-const { errors } = require('celebrate');
-const {login, createUser} = require('./controllers/users');
-const usersRouter = require('./routes/users');
-const cardsRouter = require('./routes/cards');
-const { HttpStatus, HttpResponseMessage } = require('./enums/http');
+const mongoose = require("mongoose");
+const auth = require("./middleware/auth");
+const { celebrate, Joi } = require("celebrate");
+const validator = require("validator");
+const { errors } = require("celebrate");
+const { login, createUser } = require("./controllers/users");
+const usersRouter = require("./routes/users");
+const cardsRouter = require("./routes/cards");
+const { HttpStatus, HttpResponseMessage } = require("./enums/http");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const validateURL = (value, helpers) => {
   if (validator.isURL(value)) {
     return value;
   }
-  return helpers.error('string.uri');
+  return helpers.error("string.uri");
 };
 
 mongoose.connect("mongodb://localhost:27017/aroundb", {
@@ -23,47 +24,47 @@ mongoose.connect("mongodb://localhost:27017/aroundb", {
 });
 
 app.use(express.json());
-app.post('/signin',celebrate({
-  body: Joi.object().keys({
-    email:  Joi.string().email().required(),
-    password:  Joi.string().required().min(8)
+app.use(requestLogger); //logger de solicitud
+app.post(
+  "/signin",
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().email().required(),
+      password: Joi.string().required().min(8),
+    }),
   }),
-}), login);
-
-app.post('/signup',celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().custom(validateURL),
-    email:  Joi.string().email().required(),
-    password:  Joi.string().required().min(8)
+  login
+);
+app.post(
+  "/signup",
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().custom(validateURL),
+      email: Joi.string().email().required(),
+      password: Joi.string().required().min(8),
+    }),
   }),
-}), createUser);
-
+  createUser
+);
 app.use(auth);
 app.use("/users", usersRouter);
-
 app.use("/cards", cardsRouter);
-
-
 app.use((req, res) => {
   return res
     .status(HttpStatus.BAD_REQUEST)
     .send(HttpResponseMessage.BAD_REQUEST);
 });
-
+app.use(errorLogger); // habilitar el logger de errores
 app.use(errors());
-
 app.use((err, req, res, next) => {
-   const { statusCode = HttpStatus.INTERNAL_SERVER_ERROR, message } = err;
-   res
-     .status(statusCode)
-     .send({
-       message: statusCode === 500
-         ? 'Se ha producido un error en el servidor'
-         : message
-     });
- });
+  const { statusCode = HttpStatus.INTERNAL_SERVER_ERROR, message } = err;
+  res.status(statusCode).send({
+    message:
+      statusCode === 500 ? "Se ha producido un error en el servidor" : message,
+  });
+});
 
 const { PORT = 3000 } = process.env;
 app.listen(PORT, () => {
